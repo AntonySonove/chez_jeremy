d5cc90b32d7463862ce07a540bb9584b1b937ea5
antony <asonove@gmail.com>
2025-04-04 17:52:41 +0200

    save

---
+++ script/header.js
@@ -0,0 +1,15 @@
+const burgerMenuButton= document.getElementById("burgerMenuButton");
+const dropdownHeader=document.getElementById("dropdownHeader");
+
+burgerMenuButton.addEventListener("click",(event)=>{
+    dropdownHeader.classList.toggle("showDropdown")
+    event.stopPropagation(); //? empêche de clique de se propager au document
+});
+
+document.addEventListener("click", (event)=>{
+    if(!dropdownHeader.contains(event.target) && !burgerMenuButton.contains(event.target)){
+        dropdownHeader.classList.remove("showDropdown")
+    }
+})
+
+
